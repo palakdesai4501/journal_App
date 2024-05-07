457a315b8f78372a6115ad450b5dc3b2d6a42493
Palak <palak.desai4501@gmail.com>
2024-05-06 20:30:30 -0400

    Added User Repository

---
+++ src/main/java/com/palak/journalApp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.palak.journalApp.repository;
+
+import com.palak.journalApp.entity.JournalEntry;
+import com.palak.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId>{
+
+}
