fafd204a2dff0047ff802560f8f938285d43ce13
Palak <palak.desai4501@gmail.com>
2024-05-06 20:28:34 -0400

    Added DBRef

--- src/main/java/com/palak/journalApp/entity/User.java
+++ src/main/java/com/palak/journalApp/entity/User.java
@@ -6,9 +6,12 @@ import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "users")
 @Data
@@ -21,4 +24,6 @@ public class User {
 		private String userName;
 		@NonNull
 		private String password;
+		@DBRef
+		private List<JournalEntry> journalEntries = new ArrayList<>();
 }
