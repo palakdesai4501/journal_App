a3833592c5377683f8ed4d87800c9e379223ce3a
Palak <palak.desai4501@gmail.com>
2024-05-06 21:22:08 -0400

    Added get, post, put mapping

--- src/main/java/com/palak/journalApp/controller/UserController.java
+++ src/main/java/com/palak/journalApp/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.palak.journalApp.controller;
 
 import com.palak.journalApp.entity.JournalEntry;
+import com.palak.journalApp.entity.User;
 import com.palak.journalApp.service.JournalEntryService;
+import com.palak.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +17,28 @@ import java.util.Optional;
 @RequestMapping("/journal")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
 
+	@GetMapping
+	public List<User> getAllUsers(){
+		return userService.getAll();
+	}
 
+	@PostMapping
+	public void createUser(@RequestBody User user){
+		userService.saveEntry(user);
+	}
+
+	@PutMapping
+	public ResponseEntity<?> updateUser(@RequestBody User user){
+		User userInDb = userService.findByUserName(user.getUserName());
+		if (userInDb != null){
+			userInDb.setUserName(user.getUserName());
+			userInDb.setPassword(user.getPassword());
+			userService.saveEntry(userInDb);
+		}
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
 }
